a5757ad2f025e441bc5dea29fed21462c1688416
Tashrik Anam <anampartho@users.noreply.github.com>
2024-08-21 23:03:31 +0600

    fix(duplicate): fix duplicate keymaps and update comments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,26 +1,23 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
--- Increment/Decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
-
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Jumplist
 keymap.set("n", "<C-m>", "<C-i>", opts)
 
--- increment/decrement numbers
+-- Increment/Decrement numbers
 keymap.set("n", "+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
--- window management
+-- Window management
 keymap.set("n", "sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
+-- Tab management
 keymap.set("n", "to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
